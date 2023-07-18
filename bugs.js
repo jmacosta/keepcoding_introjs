cfff02b0ee25673623a41b8749da9e12c9fa1aac
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-07-18 12:35:15 +0200

    fix: error in condition for loop

--- bugs.js
+++ bugs.js
@@ -4,7 +4,7 @@
 
 const calcularPromedio = (numeros) => {
   let sumaTotal = 0;
-  for (let i = 0; i <= numeros.length; i++) {
+  for (let i = 0; i < numeros.length; i++) {
     sumaTotal += numeros[i];
   }
   const promedio = sumaTotal / numeros.length;
