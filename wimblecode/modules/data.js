87318ae0c8ed1ead1266351bc1f4052de006fbd8
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-07-27 22:59:42 +0200

    feat: data structures

---
+++ wimblecode/modules/data.js
@@ -0,0 +1,8 @@
+export const listPlayers = [
+  "Alberto Casero",
+  "David Jiménez",
+  "Javier de Miguel",
+  "Eduardo Aguilar",
+];
+export const scoreSystem = [0, 15, 30, 40, "Ganas"];
+export const MAX_ROUND = 7;
