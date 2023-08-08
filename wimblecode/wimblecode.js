66cf4a0e024583662ee9bd46e46d1bbf088a5bbd
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-08-08 23:18:28 +0200

    feat: WIP functions for tournament

--- wimblecode/wimblecode.js
+++ wimblecode/wimblecode.js
@@ -1,6 +1,32 @@
 import { listPlayers } from "./modules/data.js";
-import { createMatch, playOff } from "./modules/functions.js";
+import {
+  createMatch,
+  hasAnyNull,
+  initPlayers,
+  playOff,
+} from "./modules/functions.js";
+const tournament = (listPlayers) => {
+  const allPlayers = initPlayers(listPlayers);
 
-const pairing = playOff(listPlayers);
-const game = createMatch(pairing[0], pairing[1]);
-game.debugPlayers();
+  //while (hasAnyNull(allPlayers) || !hasOnlyOneWinner(allPlayers)) {
+  while (hasAnyNull(allPlayers)) {
+    const [playerA, playerB] = playOff(
+      allPlayers.filter((element) => element.winnerMatch === null)
+    );
+    const game = createMatch(playerA.name, playerB.name);
+    while (!game.getWinner()) {
+      game.pointWonBy(Math.floor(Math.random() * 2) + 1);
+      game.debugPlayers();
+    }
+    allPlayers.forEach((element) => {
+      if (element.name === playerA.name || element.name === playerB.name) {
+        element.winnerMatch = element.name === game.getWinner();
+      }
+    });
+    console.log(allPlayers);
+  }
+  // to do clean losers and play again
+  //};
+};
+
+tournament(listPlayers);
