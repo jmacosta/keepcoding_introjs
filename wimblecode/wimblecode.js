d93452406e2026714af5144102204b6d8f451ed4
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-08-10 21:16:34 +0200

    feat: tournament winner and loops for players

--- wimblecode/wimblecode.js
+++ wimblecode/wimblecode.js
@@ -2,31 +2,46 @@ import { listPlayers } from "./modules/data.js";
 import {
   createMatch,
   hasAnyNull,
+  hasOnlyOneWinner,
   initPlayers,
   playOff,
 } from "./modules/functions.js";
 const tournament = (listPlayers) => {
-  const allPlayers = initPlayers(listPlayers);
+  let tournamentWinner = "";
+  let allPlayers = initPlayers(listPlayers);
 
-  //while (hasAnyNull(allPlayers) || !hasOnlyOneWinner(allPlayers)) {
-  while (hasAnyNull(allPlayers)) {
-    const [playerA, playerB] = playOff(
-      allPlayers.filter((element) => element.winnerMatch === null)
-    );
-    const game = createMatch(playerA.name, playerB.name);
-    while (!game.getWinner()) {
-      game.pointWonBy(Math.floor(Math.random() * 2) + 1);
-      game.debugPlayers();
-    }
-    allPlayers.forEach((element) => {
-      if (element.name === playerA.name || element.name === playerB.name) {
-        element.winnerMatch = element.name === game.getWinner();
+  while (hasAnyNull(allPlayers) && !hasOnlyOneWinner(allPlayers)) {
+    while (hasAnyNull(allPlayers)) {
+      const [playerA, playerB] = playOff(
+        allPlayers.filter((element) => element.winnerMatch === null)
+      );
+      const game = createMatch(playerA.name, playerB.name);
+      let gameScore = game.getGameScore();
+      let matchScore = game.getMatchScore();
+      while (!game.getWinner()) {
+        game.pointWonBy(Math.floor(Math.random() * 2) + 1);
+        console.log(game.getCurrentRoundScore());
+        if (matchScore !== game.getMatchScore()) {
+          console.log(`win this game ${game.getMatchScore()}`);
+          matchScore = game.getMatchScore();
+        } else if (gameScore !== game.getGameScore()) {
+          console.log(`Round score ${game.getGameScore()}`);
+          gameScore = game.getGameScore();
+        }
       }
-    });
-    console.log(allPlayers);
+      allPlayers.forEach((element) => {
+        if (element.name === playerA.name || element.name === playerB.name) {
+          element.winnerMatch = element.name === game.getWinner();
+        }
+      });
+      tournamentWinner = game.getWinner();
+    }
+    if (allPlayers.length > 2) {
+      allPlayers = allPlayers.filter((element) => element.winnerMatch);
+      allPlayers.map((element) => (element.winnerMatch = null));
+    }
   }
-  // to do clean losers and play again
-  //};
+  return tournamentWinner;
 };
 
-tournament(listPlayers);
+console.log(`Tournament winner is ${tournament(listPlayers)}`);
